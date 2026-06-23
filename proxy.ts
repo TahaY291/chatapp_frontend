22f79fae775e3a080a73fb6e20ff1565a5971874
TahaY291 <mtahayasin07@gmail.com>
2026-06-23 10:20:08 +0500

    proxy file is commented to check the backend is working

--- proxy.ts
+++ proxy.ts
@@ -12,18 +12,18 @@ export function proxy (req : NextRequest) {
     const email = req.cookies.get('email')?.value
     console.log("Proxy middleware:", { email })
 
-  if (!accessToken && protectedRoutes.some(r => pathname.startsWith(r))) {
-        return NextResponse.redirect(new URL("/login", req.url))
-    }
+//   if (!accessToken && protectedRoutes.some(r => pathname.startsWith(r))) {
+//         return NextResponse.redirect(new URL("/login", req.url))
+//     }
 
-if (accessToken &&  isVerified !== "true" && verifiedOnlyRoutes.some(r => pathname.startsWith(r))) {
-    const url = new URL("/verify-otp", req.url)
-    if (email) url.searchParams.set("email", email)
-    return NextResponse.redirect(url)
-}
-      if (accessToken && isVerified === "true"  && authRoutes.some(r => pathname.startsWith(r))) {
-        return NextResponse.redirect(new URL("/chats", req.url))
-    }
+// if (accessToken &&  isVerified !== "true" && verifiedOnlyRoutes.some(r => pathname.startsWith(r))) {
+//     const url = new URL("/verify-otp", req.url)
+//     if (email) url.searchParams.set("email", email)
+//     return NextResponse.redirect(url)
+// }
+//       if (accessToken && isVerified === "true"  && authRoutes.some(r => pathname.startsWith(r))) {
+//         return NextResponse.redirect(new URL("/chats", req.url))
+//     }
      return NextResponse.next()
 }
 export const config = {
